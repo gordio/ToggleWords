fcd45a2fc1e1098893f3796b05fe597a025e5f8c
Gordio <Gordio@ya.ru>
2011-10-30 03:42:19 +0200

    First release

---
+++ ToogleBool.py
@@ -0,0 +1,23 @@
+import sublime, sublime_plugin
+
+bool_dict = [
+	('true', 'false'),
+	('on',   'off'),
+	('yes',  'no'),
+	('0',    '1'),
+	('+',    '-'),
+	('&&',   '||'),
+	('<',    '>'),
+]
+
+class ToggleVarCommand(sublime_plugin.TextCommand):
+	def run(self, view):
+		word_region = self.view.word(self.view.sel()[0])
+		word = self.view.substr(word_region)
+
+		for bool_word in bool_dict:
+			if word == bool_word[0]:
+				self.view.replace(view, word_region, bool_word[1])
+			if word == bool_word[1]:
+				self.view.replace(view, word_region, bool_word[0])
+
