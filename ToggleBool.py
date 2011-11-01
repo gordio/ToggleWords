5df04bf9599e0e400a79853b499193effa416f94
Gordio <Gordio@ya.ru>
2011-11-01 13:59:53 +0200

    Change plane.

---
+++ ToggleBool.py
@@ -0,0 +1,41 @@
+import sublime, sublime_plugin
+
+bool_dict = [
+	('true', 'false'),
+	('yes', 'no'),
+	('on', 'off'),
+	('0', '1')
+]
+
+class ToggleBoolCommand(sublime_plugin.TextCommand):
+	def toggle_bool(self, view, region):
+		word = self.view.substr(region)
+
+		for bool_word in bool_dict:
+			if word == bool_word[0]:
+				self.view.replace(view, region, bool_word[1])
+				continue
+			if word == bool_word[1]:
+				self.view.replace(view, region, bool_word[0])
+				continue
+
+			# For first letter apper
+			if word == bool_word[0].capitalize():
+				self.view.replace(view, region, bool_word[1].capitalize())
+				continue
+			if word == bool_word[1].capitalize():
+				self.view.replace(view, region, bool_word[0].capitalize())
+				continue
+
+			# For all letter's apper
+			if word == bool_word[0].upper():
+				self.view.replace(view, region, bool_word[1].upper())
+				continue
+			if word == bool_word[1].upper():
+				self.view.replace(view, region, bool_word[0].upper())
+				continue
+		
+	def run(self, view):
+		for region in self.view.sel():
+			word_region = self.view.word(region)
+			self.toggle_bool(view, word_region)
